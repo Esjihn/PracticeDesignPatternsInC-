8d62213661fcd1e98df0da1ffaaf11e9f5d8cd22
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-06-21 11:28:00 -0400

    Add liskov and interface segregation

---
+++ SOLID/InterfaceSegregation.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID
+{
+    public class InterfaceSegregation
+    {
+        // slim down interfaces so that implementors of those interfaces
+        // do not need to implement modules they do not need.
+        public class Document
+        {
+
+        }
+
+        // large interface works fine if you have a multifunction printer
+        public interface IMachine
+        {
+            void Print(Document d);
+            void Fax(Document d);
+            void Scan(Document d);
+        }
+
+        // interface segregration break down
+        public interface IPrint
+        {
+            void Print(Document d);
+        }
+        public interface IFax
+        {
+            void Fax(Document d);
+        }
+        public interface IScan
+        {
+            void Scan(Document d);
+        }
+
+        public class MultifunctionPrinter : IMachine
+        {
+            public void Print(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Fax(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // cant fax or scan only print. violation!!
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Print(Document d)
+            {
+                // works
+            }
+
+            public void Fax(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class PhotoCopier : IPrint, IScan
+        {
+            public void Print(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // hierarchical
+        public interface IMultiFunctionDevice : IScan, IPrint
+        {
+
+        }
+
+        public class MultiFunctionMachine : IMultiFunctionDevice
+        {
+            // delegating print
+            public void Print(Document d)
+            {
+                _printer.Print(d);
+            }
+
+            // delegating scan
+            public void Scan(Document d)
+            {
+                _scanner.Scan(d);
+            }
+
+            private readonly IPrint _printer;
+            private readonly IScan _scanner;
+
+            // decorator pattern
+            public MultiFunctionMachine(IPrint printer, IScan scanner)
+            {
+                this._printer = printer ?? throw new ArgumentNullException(nameof(printer));
+                this._scanner = scanner ?? throw new ArgumentNullException(nameof(scanner));
+
+                this._scanner = scanner;
+                this._printer = printer;
+            }
+        }
+
+        // change to main to run.
+        public static void none(string[] args)
+        {
+
+        }
+    }
+}
--- SOLID/LiskovSubstitution.cs
+++ SOLID/LiskovSubstitution.cs
@@ -1,14 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace SOLID
 {
+    public class Rectangle
+    {
+        // add virtual to fix
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+            
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    // Square is-a rectangle
+    public class Square : Rectangle
+    {
+        // substitute override for new to fix
+        public override int Width
+        {
+            set
+            {
+                base.Width = base.Height = value;
+            }
+        }
+
+        public override int Height
+        {
+            set
+            {
+                base.Width = base.Height = value;
+            }
+        }
+    }
+
     public class LiskovSubstitution
     {
-        public static void Main(string[] args)
+        public static int Area(Rectangle r)
         {
+            return r.Width * r.Height;
+        }
 
+        // change to main to run.
+        public static void none(string[] args)
+        {
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+            
+            //Rectangle sq = new Square(); violates the liskov substitution principle if this breaks functionality
+            // should be able to "upcast" to your base type and preserve functionality. The explicit issue here is that only
+            // the width is getting changed. 
+            
+            // with virtual and override instead of normal fields and "new" setters even with a rectangle reference and only
+            // width set the functionality is preserved for square. 
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
         }
     }
 }
