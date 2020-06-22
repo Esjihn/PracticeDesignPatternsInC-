9fc0be3cc0e65c2c56b5eb446bf610ea1adbc9cd
Esjihn <sysnom@gmail.com>
2020-06-22 07:01:22 -0400

    add IDatabase interface

---
+++ Singletons/SingletonImplementation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singletons
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }   
+
+    
+    public class SingletonImplementation
+    {
+        // change to Main to run.
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
