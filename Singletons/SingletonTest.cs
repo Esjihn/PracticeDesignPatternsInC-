9098932e35708e7a750d4082de54f49114836845
Matthew Miller <sysnom@hotmail.com>
2020-06-22 16:18:49 -0400

    finished singleton coding exercise test.

--- Singletons/SingletonTest.cs
+++ Singletons/SingletonTest.cs
@@ -6,13 +6,29 @@ using NUnit.Framework;
 
 namespace Singletons
 {
+    // non singleton test
+    
     public class Person
     {
-        public string Name { get; set; }
+        public static string Name { get; set; }
 
         public Person(string name)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        private Func<object> nonStaticFunc = new Func<object>(() => new object());
+
+        public Func<object> NonStaticFunc()
+        {
+            return nonStaticFunc;
+        }
+
+        private static Func<object> getFunc = new Func<object>(() => GetFunc());
+
+        public static object GetFunc()
+        {
+            return getFunc;
         }
     }
 
@@ -21,16 +37,26 @@ namespace Singletons
 
         public static bool IsSingleton(Func<object> func)
         {
-            // todo
-
+            // use this to test singleton
+            //var func2 = new Func<object>(Person.GetFunc);
+            
+            // use this to test non singleton.
+            var func2 = new Func<object>(() => new object());
+            
+            if (func == func2)
+                return true;
+         
+            return false;
         }
 
         // change to Main to run.
         public static void Main(string[] args)
         {
+            Person p = new Person("Matt");
             
-            // pb.actions or static PersonBuilder.actions
-            if (IsSingleton())
+            // use Person.GetFunc as IsSingleton parameter for singleton
+            // or create new Person object and then pass p.NonStaticFunc()
+            if (IsSingleton(p.NonStaticFunc()))
             {
                 Console.WriteLine("Yes, this is a singleton");
             }
