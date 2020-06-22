f66d469deb786de9f5828b038f67df983c4d1650
Esjihn <sysnom@gmail.com>
2020-06-22 06:55:43 -0400

    comments

--- Singletons/Program.cs
+++ Singletons/Program.cs
@@ -4,7 +4,21 @@ namespace Singletons
 {
     public class Program
     {
-        // Singletons
+        // Singletons - A component which is only instantiated once. 
+        // (A design pattern everyone loves to hate.. but is it really that bad?)
+        // "When discussing which patterns to drop, we found that we still love them all.
+        // (Not really-- I'm in favor of dropping Singleton. Its use is almost always a
+        // design smell. -Erich Gamma (Gamma Categorization [structural, behavioral, creational] author)
+
+        // Motivation
+        // For some components it only makes sense to have one in the system.
+        // 1) Database repository singleton (no point in having more than one).
+        // 2) Object factory (factory isn't supposed to have any state, you only need one).
+        // 3) e.g. the constructor call is expensive
+        //      b) We only do it once.
+        //      c) We provide everyone with the same instance.
+        // 4) Want to prevent anyone (clients) from creating additional copies.
+        // 5) Need to take care of lazy (call-by-need) instantiation and thread safety.
 
         // change to Main to run. 
         public static void none(string[] args)
