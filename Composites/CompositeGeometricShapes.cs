a3468198090750c9d24b9dc7aab5e8e9d21b869e
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-06-22 22:37:30 -0400

    NeuralNetwork implementation

--- Composites/CompositeGeometricShapes.cs
+++ Composites/CompositeGeometricShapes.cs
@@ -1,17 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Composites
 {
+    // Objects composing of other objects of its own type.
+    public class GraphicObject
+    {
+        private string _name = "Group";
 
+        public virtual string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Color;
+        private Lazy<List<GraphicObject>> children 
+            = new Lazy<List<GraphicObject>>();
+
+        public List<GraphicObject> Children
+        {
+            get { return children.Value; }
+        }
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color}")
+                .AppendLine(Name);
+
+            foreach (GraphicObject child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name
+        {
+            get { return "Circle"; }
+        }
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name
+        {
+            get { return "Square"; }
+        }
+    }
 
     public class CompositeGeometricShapes
     {
         // change to Main to run.
-        public static void Main(string[] args)
+        public static void none(string[] args)
         {
-            
+            var drawing = new GraphicObject {Name = "My Drawing"};
+            drawing.Children.Add(new Square {Color = "Red"});
+            drawing.Children.Add(new Circle {Color = "Yellow"});
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle {Color = "Blue"});
+            group.Children.Add(new Square {Color = "Blue"});
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
         }
     }
 }
---
+++ Composites/CompositeNeuralNetworks.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Composites
+{
+    // One component of machine learning is Neural networks. Composite design pattern
+    // is just one facet of neural networks.
+
+    public static class ExtensionMethods
+    {
+
+        public static void ConnectTo(this IEnumerable<Neuron> self,
+            IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+
+            foreach (Neuron from in self)
+            {
+                foreach (Neuron to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+            }
+        }
+    }
+
+    // Neuron is a scalar value
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            // helps scalar values get treated the same as composite values
+            // by returning itself
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+        
+    }
+
+    public class CompositeNeuralNetworks
+    {
+        // change to Main to run
+        public static void Main(string[] args)
+        {
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+
+            neuron1.ConnectTo(neuron2); // 1
+
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+
+            // 4 every neuron and layer
+
+            // works now as IEnumerable and yield returning itself
+            neuron1.ConnectTo(layer2);
+        }
+    }
+}
