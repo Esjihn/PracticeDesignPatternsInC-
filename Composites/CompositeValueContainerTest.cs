706b2faffc054f7af8ac2e1e4ab9a270d80aaf9a
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-06-22 22:57:08 -0400

    add composite coding exercise shell will finish tomorrow.

---
+++ Composites/CompositeValueContainerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composites
+{
+
+    public class CompositeValueContainerTest
+    {
+        // change to Main to run.
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
