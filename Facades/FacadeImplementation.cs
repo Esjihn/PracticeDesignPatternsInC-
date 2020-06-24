c33d072580db10010f259f2fac906c971004ee9b
Esjihn <sysnom@gmail.com>
2020-06-23 20:19:08 -0400

    Facade implementation

---
+++ Facades/FacadeImplementation.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facades
+{
+    public abstract class EnginePartDescription
+    {
+        public abstract override string ToString();
+    }
+
+
+    public class EngineFacade 
+    {
+        EngineBlock engineBlock = new EngineBlock();
+        Pistons piston = new Pistons();
+        CylinderHead cylinderHead = new CylinderHead();
+        CrankShaft crankShaft = new CrankShaft();
+        CamShaft camShaft = new CamShaft();
+        Valves valves = new Valves();
+        OilPan oilPan = new OilPan();
+
+
+        public void BuildEngine()
+        {
+            Console.WriteLine(engineBlock.ToString());
+            Console.WriteLine(piston.ToString());
+            Console.WriteLine(cylinderHead.ToString());
+            Console.WriteLine(crankShaft.ToString());
+            Console.WriteLine(camShaft.ToString());
+            Console.WriteLine(valves.ToString());
+            Console.WriteLine(oilPan.ToString());
+        }
+
+        public void BuildShafts()
+        {
+            Console.WriteLine(crankShaft.ToString());
+            Console.WriteLine(camShaft.ToString());
+        }
+
+        private class EngineBlock : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building engine block...";
+            }
+        }
+
+        private class Pistons : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building pistons...";
+            }
+        }
+
+        private class CylinderHead : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building cylinder head...";
+            }
+        }
+
+        private class CrankShaft : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building crankshaft...";
+            }
+        }
+
+        private class CamShaft : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building camshaft...";
+            }
+        }
+
+        private class Valves : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building valves...";
+            }
+        }
+
+        private class OilPan : EnginePartDescription
+        {
+            public override string ToString()
+            {
+                return "Building oil pan...";
+            }
+        }
+    }
+
+    public class FacadeImplementation
+    {
+        // change to Main to run.
+        public static void Main(string[] args)
+        {
+            EngineFacade ef = new EngineFacade();
+            ef.BuildEngine();
+            Console.WriteLine();
+            ef.BuildShafts();
+        }
+    }
+}
---
+++ Facades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Facades
+{
+    // FAÇADE - Provides a simple, easy to understand/user interface over a large and sophisticated
+    // body of code. 
+    // Exposing several components through a single interface.
+
+    // Motivation
+    // 1) Balancing complexity and presentation/usability (like a house)
+    // 2) Typical home
+    //      b) Many subsystems (electrical, sanitation and plumbing)
+    //      c) Complex internal structure (i.e. floor built [layers])
+    //      d) End user is not exposed to internals
+    // 3) Same with software. (sometimes consumers just want something simple to work with.
+    //      b) Many systems working to provide flexibility, but..
+    //      c) API consumers want it to just work (couple simple API calls) 
+    //          i) i.e Console complex internals, buffers, re-allocations, overruns, advanced settings
+    //                      but Console.Write, WriteLine, Read, ReadLine are simple and easy to use.  
+
+    // Summary
+    // 
+
+    public class Program
+    {
+        // change to Main to run.
+        public static void none(string[] args)
+        {
+            
+        }
+    }
+}
