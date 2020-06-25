55dfbe66ddbfc642dbee490414bc00b368ae6735
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-06-24 21:56:00 -0400

    Finished flyweights

--- Flyweights/FlyweightFormattingTest.cs
+++ Flyweights/FlyweightFormattingTest.cs
@@ -6,39 +6,48 @@ namespace Flyweights
 {
     public class Sentence
     {
-        private readonly string _plainText;
-
         private readonly WordToken[] _tokens;
-        private string[] _strTokens;
+        private readonly string[] _strTokens;
 
         public Sentence(string plainText)
         {
-            _plainText = plainText;
-            _strTokens = _plainText.Split(' ');
+            _strTokens = plainText.Split(' ');
+            _tokens = new WordToken[_strTokens.Length];
         }
 
         public WordToken this[int index]
         {
             get
             {
+                if (index > _tokens.Length - 1) return new WordToken();
+
+                _tokens[index] = new WordToken();
+                _tokens[index].Capitalize = true;
+
                 return _tokens[index];
             }
         }
 
         public override string ToString()
         {
-            // todo return modified plain text.
-            return _plainText;
+            StringBuilder sb = new StringBuilder();
+
+            for (var i = 0; i < _strTokens.Length; i++)
+            {
+                if (this._tokens[i] != null && this._tokens[i].Capitalize)
+                {
+                    this._strTokens[i] = _strTokens[i].ToUpperInvariant();
+                }
+
+                sb.Append(_strTokens[i] + " ");
+            }
+
+            return sb.ToString();
         }
 
         public class WordToken
         {
             public bool Capitalize;
-
-            public WordToken(bool capitalize)
-            {
-                Capitalize = capitalize;
-            }
         }
     }
 
