40a841e25d9f1938153e6a95905f1a7136e727c8
Matthew Miller <sysnom@hotmail.com>
2020-06-26 09:20:59 -0400

    SOLID summary complete

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -25,9 +25,17 @@ namespace SOLID
     // New classes can produce undesirable affects when they are used in existing program modules. 
     // Stability. 
 
-    // Interface segregation Principle
+    // Interface segregation Principle - No client should be forced to depend on methods it does not use. 
 
-    // Dependency inversion Principle
+    // Motivation 
+    // Interface version of SRP, iSRP 
+
+    // Dependency inversion Principle - High level parts should not depend on low level systems directly instead they 
+    // should depend on some sort of abstraction (interface constructor injection etc)
+    
+    // Motivation 
+    // Low level parts can define how underlying data structures are managed because they themselves are never directly
+    // exposed to the high level parts. 
 
     public class Program
     {
