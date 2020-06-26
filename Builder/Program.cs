a925168d28cc3bd7fd95866caa329135732d9064
Matthew Miller <sysnom@hotmail.com>
2020-06-26 08:57:14 -0400

    Refactor and SOLID summary additions

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -5,7 +5,8 @@ namespace Builder
     // Creational
     // Builder - When piecewise object construction is complicated provide
     // an API for doing it succinctly (clearly and briefly expressed)
-    // TODO
+    
+    // Motivation
     // 1) Some objects are simple and can be created in a single
     // constructor call
     // 2) Other objects require a lot of ceremony to create. 
--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -4,6 +4,31 @@ using System.IO;
 
 namespace SOLID
 {
+    // SOLID
+    // Single Responsibility Principle - Classes and methods should have only one responsibility. 
+    
+    // Motivation
+    // Easier testability
+    // Code-base is easier to understand.
+
+    // Open / Closed Principle - Classes should be closed for modification and open for extension.
+    
+    // Motivation
+    // No need to re deploy already existing modules. 
+    // Easier testability
+    // Stability
+    
+    // Liskov substitution Principle (Barbara Liskov) - You should be able to substitute a base type for a sub type.
+
+    // Motivation store a reference of a sub type as a base type. i.e. Square square = new Rectangle() (should be valid)
+    // New derived classes just extend without replacing the functionality of old classes
+    // New classes can produce undesirable affects when they are used in existing program modules. 
+    // Stability. 
+
+    // Interface segregation Principle
+
+    // Dependency inversion Principle
+
     public class Program
     {
 
