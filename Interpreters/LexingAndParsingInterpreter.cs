de8374c98e114989449de3c0a4c619c85f90dab3
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-06-27 19:23:00 -0400

    lexing and parsing manual creation

--- Interpreters/LexingAndParsingInterpreter.cs
+++ Interpreters/LexingAndParsingInterpreter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Interpreters
@@ -10,9 +11,43 @@ namespace Interpreters
         int Value { get; }    
     }
 
-    public class MyClass
+    // Integer for parsing.
+    public class Integer : IElement
     {
-        
+        public int Value { get; }
+
+        public Integer(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class BinaryOperation : IElement
+    {
+        public enum Type
+        {
+            Addition, Subtraction
+        }
+
+        public Type MyType;
+        public IElement Left, Right;
+
+
+        public int Value
+        {
+            get
+            {
+                switch (MyType)
+                {
+                    case Type.Addition:
+                        return Left.Value + Right.Value;
+                    case Type.Subtraction:
+                        return Left.Value - Right.Value;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
     }
 
     public class Token
@@ -82,15 +117,69 @@ namespace Interpreters
             return result;
         }
 
+        static IElement Parse(IReadOnlyList<Token> tokens)
+        {
+            var result = new BinaryOperation();
+            bool haveLHS = false;
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                var token = tokens[i];
+
+                // look at the type of token
+                switch (token.MyType)
+                {
+                    case Token.Type.Integer:
+                        var integer = new Integer(int.Parse(token.Text));
+                        if (!haveLHS)
+                        {
+                            result.Left = integer;
+                            haveLHS = true;
+                        }
+                        else
+                        {
+                            result.Right = integer;
+                        }
+                        break;
+                    case Token.Type.Plus:
+                        result.MyType = BinaryOperation.Type.Addition;
+                        break;
+                    case Token.Type.Minus:
+                        result.MyType = BinaryOperation.Type.Subtraction;
+                        break;
+                    case Token.Type.Lparen:
+                        int j = i;
+                        for (; j < tokens.Count; ++j)
+                            if (tokens[j].MyType == Token.Type.Rparen)
+                                break; // found it!
+                        // process subexpression w/o opening (
+                        var subexpression = tokens.Skip(i + 1).Take(j - i - 1).ToList();
+                        var element = Parse(subexpression);
+                        if (!haveLHS)
+                        {
+                            result.Left = element;
+                            haveLHS = true;
+                        }
+                        else result.Right = element;
+                        i = j; // advance
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            return result;
+        }
 
         // change to Main to run.
         public static void Main(string[] args)
         {
-            // separate tokens ( 13 + 4 ) - ( 12 etc..
+            // separate tokens ( 13 + 4 ) - ( 12 etc.. // i(minus) part makes it a binary operation.
             string input = "(13+4)-(12+1)";
 
             var tokens = Lex(input);
             Console.WriteLine(string.Join("\t", tokens));
+
+            var parsed = Parse(tokens);
+            Console.WriteLine($"{input} = {parsed.Value}");
         }
     }
 }
