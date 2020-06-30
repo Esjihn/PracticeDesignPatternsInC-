98be0a42f4ccee7aa2525f67050f259e8cf07374
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-06-30 10:24:29 -0400

    Finished Interpreter Design Pattern

--- Interpreters/InterpreterExerciseTest.cs
+++ Interpreters/InterpreterExerciseTest.cs
@@ -20,17 +20,65 @@ namespace Interpreters
 
         public int Calculate(string expression)
         {
-            // todo
+            int result = 0;
+            int[] calcArray = new int[expression.Length];
+
             for (var index = 0; index < expression.Length; index++)
             {
                 char c = expression[index];
-                Variables.Add(c, index);
+
+                if (index > 1)
+                {
+                    char c1 = expression[index];
+                    char c2 = expression[index - 1];
+
+                    bool b1C1 = int.TryParse(c1.ToString(), out int c1Int);
+                    calcArray[index] = c1Int;
+                    bool b2C2 = int.TryParse(c2.ToString(), out int c2Int);
+                    calcArray[index] = c2Int;
+                    // If there are consecutive variables 'xy' return 0 per acceptance criteria.
+                    if (!b1C1 && !b2C2 
+                              && c1 != '+' && c1 != '-'
+                              && c2 != '+' && c2 != '-') return 0;
+                }
+
+                // if char c is a number do not add to Variables.
+                if (int.TryParse(c.ToString(), out int possibleResult))
+                    continue;
+
+                // if char c is addition or subtraction symbol do not add to Variables.
+                if (c != '+' && c != '-')
+                    Variables.TryAdd(c, index);
             }
 
+            if (Variables.Count == 1)
+            {
+                foreach (KeyValuePair<char, int> keyValuePair in Variables)
+                {
+                    return keyValuePair.Value;
+                }
+            }
 
+            if (Variables.Count == 0 || Variables.Count == 1)
+            {
+                foreach (char c in expression)
+                {
+                    bool addition = expression.Contains("+");
+                    bool subtraction = expression.Contains("-");
 
+                    if (addition && c != '+' && c != '-')
+                    {
+                        result += int.Parse(c.ToString());
+                    }
 
-            return 0; // change to return result;
+                    if (subtraction && c != '+' && c != '-')
+                    {
+                        result -= int.Parse(c.ToString());
+                    }
+                }
+            }
+            
+            return result; 
         }
     }
     
@@ -45,8 +93,11 @@ namespace Interpreters
             // Calculate("10-2-x") when x=3 is in Variables should return 5
 
             ExpressionProcessor exp = new ExpressionProcessor();
-            exp.Calculate("1+2+3");
-
+            Console.WriteLine(exp.Calculate("1+2+3")); // 6
+            exp.Variables.Clear();
+            Console.WriteLine(exp.Calculate("1+2+xy")); // 0
+            exp.Variables.Clear();
+            Console.WriteLine(exp.Calculate("10-2-x")); // 5
         }
     }
 }
--- Interpreters/Program.cs
+++ Interpreters/Program.cs
@@ -19,6 +19,15 @@ namespace Interpreters
     //      e) Regular expressions (state machine)
     // 3) Turning strings into OOP based structures in a complicated process.
 
+    // Summary
+    // 1) Barring simple cases, an interpreter acts in two stages.
+    // 2) Lexing - turns text into a set of tokens.
+    //      b) i.e (3*(4+5) -> Lit[3] Star LParent Lit[4] Plus Lit[5] Rparen 'tokens'
+    // 3) Parsing - tokens into meaningful constructs
+    //      b) MultiplicationExpression[Integer[3], AdditionExpression[Integer[4], Integer[5]]] (pemdas)
+    //      c) 'abstract syntax tree
+    //      d) Parsed data can then be traversed, transformed, and interpreted. 
+
     public class Program
     {
         // change to Main to run. 
