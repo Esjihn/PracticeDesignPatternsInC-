85f83e8b7d20d41a5d6077e6149d605e046b412e
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-07-09 16:58:16 -0400

    added obervers with events

---
+++ Observers/ObserverViaEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observers
+{
+    public class FallsIllEventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        // event special construct to inform listeners
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+
+        public void CatchACold()
+        {
+            FallsIll?.Invoke(this, 
+                new FallsIllEventArgs {Address = "123 London Road"});
+        }
+    }
+
+    public class ObserverViaEvents
+    {
+        // change to Main to run.
+        public static void none(string[] args)
+        {
+            var person = new Person();
+            person.FallsIll += CallDoctor;
+            person.CatchACold();
+            person.FallsIll -= CallDoctor; // no longer publishing
+        }
+
+        private static void CallDoctor(object sender, FallsIllEventArgs e)
+        {
+            Console.WriteLine($"A doctor has been called to {e.Address}");
+        }
+    }
+}
