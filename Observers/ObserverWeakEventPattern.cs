c16ce2bd45983073ec853818528e04c56db383bc
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-07-09 19:01:51 -0400

    demonstrate WeakEvent example will only work in .NET framework not in this .NET core application

--- Observers/ObserverWeakEventPattern.cs
+++ Observers/ObserverWeakEventPattern.cs
@@ -1,16 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Channels;
 
 namespace Observers
 {
+    public class Button
+    {
+        public event EventHandler Clicked;
+
+        public void Fire()
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Window 
+    {
+        // Constructor
+        public Window(Button button)
+        {
+            // only supported in .NET Framework
+            // Would use this instead of += event handler
+            //WeakEventManager<Button, EventArgs>
+                //.AddHandler(button, "Clicked", ButtonOnClicked);
+            
+            //button.Clicked += ButtonOnClicked;
+        }
+
+        private void ButtonOnClicked(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked (window handler)");
+        }
+
+        // Destructor works in .NET Framework typically. 
+        ~Window()
+        {
+            Console.WriteLine("Window finalized");
+        }
+    }
 
     public class ObserverWeakEventPattern
     {
         // change to Main to run.
-        public static void Main(string[] args)
+        public static void none(string[] args)
         {
-            
+            var btn = new Button();
+            var window = new Window(btn);
+            var windowRef = new WeakReference(window);
+            btn.Fire();
+
+            Console.WriteLine("Setting window to null");
+            window = null;
+
+            FireGC();
+            Console.WriteLine($"is the window alive alive after GC? {windowRef.IsAlive}");
+        }
+
+        private static void FireGC()
+        {
+            Console.WriteLine("Starting GC");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            Console.WriteLine("GC is done!");
         }
     }
 }
---
+++ Observers/ObserversViaSpecialInterfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observers
+{
+
+
+    public class ObserversViaSpecialInterfaces
+    {
+        // change to Main to run.
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
