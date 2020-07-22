83bc5cf83413fb473fceb6854257923e0c94a69b
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-07-22 11:44:37 -0400

    Start State pattern

---
+++ States/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace States
+{
+    // State -
+    //      1) A pattern in which the object's behavior is determined by it's state.
+    //          An object transitions from one state to another (something needs to trigger a transition).
+    //      2) A formalized construct which manages state and transitions is called a state machine.
+
+    // Fun with Finite State Machines
+
+    // Gang of Four: Allow an object to alter its behavior when its internal state changes. 
+    // The object will appear to change its class. 
+    
+    // Motivation
+    // 1) Consider an ordinary telephone
+    // 2) What you do with it depends on the state of the phone/line
+    //      i) If it's raining or you want to make a call, you can pick it up.
+    //      ii) Phone must be off the hook to talk / make a call
+    //      iii) If you try calling someone, and it's busy, you put the handset down.
+    // 3) Changes in state can be explicit or in response to event (Observer pattern) 
+
+    // Summary
+    // 1) 
+
+    public class Program
+    {
+        // change to Main to run. 
+        public static void none(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ States/StateClassicImplementation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace States
+{
+
+
+    public class StateClassicImplementation
+    {
+        // change to Main to run.
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
