105da8a37ca4a9b2bb68a46c9204745241f766de
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-07-22 08:54:45 -0400

    finished game observer test.

--- Observers/DeclarativeEventSubscriptionsWithInterfaces.cs
+++ Observers/DeclarativeEventSubscriptionsWithInterfaces.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Autofac;
 
 namespace Observers
 {
@@ -14,15 +18,126 @@ namespace Observers
 
     public interface ISend<TEvent> where TEvent : IEvent
     {
+        event EventHandler<TEvent> Sender;
+    }
 
+    public interface IHandle<TEvent> where TEvent : IEvent
+    {
+        void Handle(object sender, TEvent args);
+    }
+
+    public class ButtonPressedEvent : IEvent
+    {
+        public int NumberOfClicks;
+    }
+
+    public class Button2 : ISend<ButtonPressedEvent>
+    {
+        public event EventHandler<ButtonPressedEvent> Sender;
+
+        public void Fire(int clicks)
+        {
+            Sender?.Invoke(this, new ButtonPressedEvent
+            {
+                NumberOfClicks = clicks
+            });
+        }
+    }
+
+    public class Logging : IHandle<ButtonPressedEvent>, IDisposable
+    {
+        public void Handle(object sender, ButtonPressedEvent args)
+        {
+            Console.WriteLine(
+                $"Button clicked {args.NumberOfClicks} times");
+        }
+
+        public void Dispose()
+        {
+            // No information about what we are subscribed to when using automatic process. 
+        }
     }
 
     public class DeclarativeEventSubscriptionsWithInterfaces
     {
         // change to Main to run.
-        public static void Main(string[] args)
+        public static void none(string[] args)
         {
+            var cb = new ContainerBuilder();
+            var assembly = Assembly.GetExecutingAssembly();
+
+            cb.RegisterAssemblyTypes(assembly)
+                .AsClosedTypesOf(typeof(ISend<>)) 
+                .SingleInstance();
+
+            cb.RegisterAssemblyTypes(assembly)
+                .Where(t => t.GetInterfaces()
+                    .Any(i =>
+                        i.IsGenericType &&
+                        i.GetGenericTypeDefinition() == typeof(IHandle<>)
+                    ))
+                .OnActivated(act =>
+                {
+                    // IHandle<Foo>
+                    // find every ISend<Foo>.Sender += act.Instance.Handle
+                    var instanceType = act.Instance.GetType();
+                    var interfaces = instanceType.GetInterfaces();
+
+                    foreach (Type i in interfaces)
+                    {
+                        if (i.IsGenericType &&
+                            i.GetGenericTypeDefinition() == typeof(IHandle<>))
+                        {
+                            // IHandle<Foo>
+                            var arg0 = i.GetGenericArguments()[0];
+
+                            // ISend<Food>, to find we need to construct first
+                            var senderType = typeof(ISend<>).MakeGenericType(arg0);
+
+                            // find every single ISend<Foo> in container
+                            // IEnumberable<IFoo> -> every instance of object that implements IFoo
+                            var allSenderTypes = typeof(IEnumerable<>)
+                                .MakeGenericType(senderType);
+
+                            // IEnumerable<ISend<Food>>
+                            var allServices = act.Context.Resolve(allSenderTypes);
+                            foreach (object service in (IEnumerable) allServices)
+                            {
+                                var eventInfo = service.GetType().GetEvent("Sender");
+                                var handleMethod = instanceType.GetMethod("Handle");
+                                if (eventInfo != null
+                                    && handleMethod != null
+                                    && eventInfo.EventHandlerType != null)
+                                {
+                                    var handler = Delegate.CreateDelegate(
+                                        eventInfo.EventHandlerType, null, handleMethod);
+
+                                    eventInfo.AddEventHandler(service, handler);
+                                }
+                            }
+                        }
+                    }
+                })
+                .SingleInstance()
+                .AsSelf();
+
+            var container = cb.Build();
+
+            var button = container.Resolve<Button2>();
             
+            // logging is getting automatically subscribed.
+            var logging = container.Resolve<Logging>();
+
+            button.Fire(1);
+            button.Fire(2);
+
+            // Problems
+            // 1) If you create a new event the during the single instance creation process it will not
+            //      automatically subscribe to those new events. 
+            // 2) IDisposable - hard to implement using declarative approach,
+            //      No information about what we are subscribed to when using automatic process. 
+            //      Instead of event senders and a handle method you would need to use a event broker
+            //      Centralized component / bus / class / broker. 
         }
     }
 }
---
+++ Observers/GameObserverTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observers
+{
+    /**
+     * Imagine a game where one or more rats can attack a player. Each individual rat has an Attack
+     * value of 1. However, rats attack as a swarm, so each rat's Attack value is equal to the
+     * total number of rats in play.
+     *
+     * Given that a rat enters play through the constructor and leaves play (dies) via its Dispose method,
+     * please implement the Game and Rat classes so that, at any point in the game the attack value of a rat
+     * is always consistent
+     *
+     * The exercise has two simple rules.
+     *      a) The Game class cannot have properties or fields. It can only contain events and methods
+     *      b) The Rat class' Attack field is strictly a field, not a property.
+     */
+    public class Game
+    {
+        // remember - no fields or properties!
+        public event EventHandler<EventArgs> RatAttackEvent;
+
+        public void OnRatAttackEvent()
+        {
+            RatAttackEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public int RatSubscriberCount()
+        {
+            if(RatAttackEvent != null)
+                return RatAttackEvent.GetInvocationList().Length;
+        
+            return 0;
+        }
+    }
+    public class Rat : IDisposable
+    {
+        public int Attack = 1;
+        private readonly Game _game;
+
+        public Rat(Game game)
+        {
+            this._game = game;
+            _game.RatAttackEvent += GameOnRatAttackEvent;
+        }
+
+        private void GameOnRatAttackEvent(object? sender, EventArgs e)
+        {
+            if (sender is Game game)
+            {
+                if (game.RatSubscriberCount() > 1)
+                    Attack++;
+                else
+                {
+                    Attack = 1;
+                }
+            }
+
+            Console.WriteLine($"{this}: {this.GetHashCode()} is now {this.Attack}");
+        }
+
+
+        public void Dispose()
+        {
+            this._game.RatAttackEvent -= GameOnRatAttackEvent;
+        }
+    }
+
+    public class GameObserverTest
+    {
+        // Change to Main to run.
+        public static void Main(string[] args)
+        {
+            var game = new Game();
+           
+            var rat = new Rat(game); // rat = 1
+            game.OnRatAttackEvent();
+            rat.Dispose(); // rat goes away
+            var rat2 = new Rat(game); // rat 2 = 1
+            game.OnRatAttackEvent(); // rat 2 not disposed (does not go away)
+            var rat3 = new Rat(game); // rat 2's attack is now ++ 1 + 1 = 2
+            game.OnRatAttackEvent(); // rat 3 and rat 2 in pot so both Rat2 and Rat 3 attack = 2 (1 + 1)
+            rat3.Dispose(); // rat 3 goes away but rat 2 remains which means only one rat in pot
+
+            
+            // Everything has been unsubscribed at this point. 
+            game.OnRatAttackEvent(); // Since only 1 rat (rat 2) remains. Then rat2 attack is reduced
+                                     // back to 1.
+        }
+    }
+}
