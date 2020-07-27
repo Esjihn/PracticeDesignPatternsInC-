001c64ed96ff24b8ea10b9c3521366f156bc1ac9
Esjihn <sysnom@gmail.com>
2020-07-26 21:05:06 -0400

    add intrusive expression visitor

--- Visitors/IntrusiveExpressionPrintingVisitor.cs
+++ Visitors/IntrusiveExpressionPrintingVisitor.cs
@@ -4,13 +4,55 @@ using System.Text;
 
 namespace Visitors
 {
+    public abstract class Expression
+    {
+        public abstract void Print(StringBuilder sb);
+    }
+
+    public class DoubleExpression : Expression
+    {
+        private double value;
+
+        public DoubleExpression(double value)
+        {
+            this.value = value;
+        }
+
+        public override void Print(StringBuilder sb)
+        {
+            sb.Append(value);
+        }
+    }
+
+    public class AdditionExpression : Expression
+    {
+        private Expression left, right;
+
+        public AdditionExpression(Expression left, Expression right)
+        {
+            this.left = left ?? throw new ArgumentNullException(nameof(left));
+            this.right = right ?? throw new ArgumentNullException(nameof(right));
+        }
+
+        public override void Print(StringBuilder sb)
+        {
+            sb.Append("(");
+            left.Print(sb);
+            sb.Append("+");
+            right.Print(sb);
+            sb.Append(")");
+        }
+    }
 
     public class IntrusiveExpressionPrintingVisitor
     {
         // change to Main to run.
         public static void Main(string[] args)
         {
-            
+            var e = new AdditionExpression(new DoubleExpression(1), new AdditionExpression(new DoubleExpression(2), new DoubleExpression(3)));
+            var sb = new StringBuilder();
+            e.Print(sb);
+            Console.WriteLine(sb);
         }
     }
 }
